63491ac0e90ce276959823fd4a006aadc90bfcb4
callrajveer <rehanshugupta1234@gmail.com>
2026-01-12 09:21:17 +0530

    task 1 commpleted

---
+++ backend/controllers/analyze.controller.js
@@ -0,0 +1,52 @@
+/**
+ * Main analysis controller
+ * Orchestrates:
+ * - Repo fetch
+ * - Filtering
+ * - Commit analysis
+ * - Scoring
+ */
+
+import jwt from "jsonwebtoken";
+import {fetchRepos} from "../services/github.service.js"
+import { filterRepos } from "../repoFilter.js";
+import { fetchCommits } from "../services/commit.service.js";
+import { calculateCommitScore } from "../services/commitScore.js";
+
+
+export async function analyzeUser(req,res){
+    try{
+       const {username} = req.params;
+
+       //extract github token from jwt 
+       const jwttoken = req.headers.authorization;
+       const {accessToken} = jwt.verify(jwttoken, process.env.JWT_SECRET);
+
+       //step 1: fetch & filter repositories
+       const repos = await fetchRepos(username,accessToken);
+       const filteredRepos = filterRepos(repos);
+
+       const results = [];
+
+       //step 2: analyze commits per repos 
+       for (const repo of filterRepos){
+        const commits = await fetchCommits(
+            username,
+            repo.name,
+            repo.defaultBranchRef.name,
+            accessToken
+        );
+        const commitScore = calculateCommitScore(commits);
+
+        results.push({
+            repo: repo.name,
+            commitScore,
+            commitsAnalyzed: commits.length
+        });
+       }
+       res.json(results);
+    }
+    catch(err){
+        res.status(500).json({ error: err.message});
+    }
+}
