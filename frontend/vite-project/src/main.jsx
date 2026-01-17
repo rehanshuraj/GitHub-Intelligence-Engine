7fa5c27bf370752185e551c33d6ec1f03cd7ea78
callrajveer <rehanshugupta1234@gmail.com>
2026-01-17 16:57:56 +0530

    Switch to HashRouter to fix Vercel 404

--- frontend/vite-project/src/main.jsx
+++ frontend/vite-project/src/main.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import { HashRouter } from "react-router-dom";
 import "./styles/global.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
+   <HashRouter>
     <App />
-  </React.StrictMode>
+   </HashRouter>
 );
